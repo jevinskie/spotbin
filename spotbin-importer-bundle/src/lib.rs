bd0c9a2c5ef35ad6a8c56a74cc719ab8eb3fb7c7
Jevin Sweval <jevinsweval@gmail.com>
2025-04-17 03:18:30 -0400

    wip

--- spotbin-importer-bundle/src/lib.rs
+++ spotbin-importer-bundle/src/lib.rs
@@ -1,5 +1,24 @@
+use objc2_core_foundation::{
+    CFMutableDictionary, CFString, HRESULT, LPVOID, REFIID, ULONG,
+};
+use std::{os::raw::c_void, ptr::null};
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct MDImporterInterfaceStruct {
+    _reserved: *mut c_void,
+    query_interface:
+        Option<unsafe extern "C-unwind" fn(*mut c_void, REFIID, *mut LPVOID) -> HRESULT>,
+    add_ref: Option<unsafe extern "C-unwind" fn(*mut c_void) -> ULONG>,
+    release: Option<unsafe extern "C-unwind" fn(*mut c_void) -> ULONG>,
+    importer_import_data:
+        Option<unsafe extern "C-unwind" fn(*mut c_void, CFMutableDictionary, CFString, CFString)>,
+}
 
 #[unsafe(no_mangle)]
-pub extern "C" fn plugin_entry() {
+pub extern "C" fn MetadataImporterPluginFactory() -> *mut c_void {
     println!("CFPlugin loaded!");
+    let a: *const c_void = null();
+    let b: *mut c_void = a.cast_mut();
+    b
 }
